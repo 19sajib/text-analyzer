21921983df75d7fd5d248a6635c70778728744e3
Md Abu Bakkar Siddiqe Sajib <contact2sajib@gmail.com>
2024-08-14 17:53:01 +0600

    boilerplate code

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AnalyzerModule } from './analyzer/analyzer.module';
 
 @Module({
-  imports: [],
+  imports: [AnalyzerModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,29 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { INestApplication, Logger, ValidationPipe } from '@nestjs/common';
+
+const setupSwagger = (app: INestApplication) => {
+	const options = new DocumentBuilder()
+		.setTitle('Text Analyzer Service API Operations')
+		.setDescription('Rest API docs')
+		.addBearerAuth({ description: 'User JWT Token', type: 'http', name: 'Authorization', bearerFormat: 'JWT' })
+		.setVersion('1.0')
+		.build();
+
+	const document = SwaggerModule.createDocument(app, options);
+	SwaggerModule.setup('api', app, document, { swaggerOptions: { persistAuthorization: true } });
+};
 
 async function bootstrap() {
+  const port = process.env.PORT || 4949;
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.setGlobalPrefix('api')
+  app.enableCors()
+  app.useGlobalPipes( new ValidationPipe())
+  const logger = new Logger('Startup');
+  setupSwagger(app);
+  await app.listen(port);
+  logger.log(`App Started on http://localhost:4949/api`);
 }
 bootstrap();
